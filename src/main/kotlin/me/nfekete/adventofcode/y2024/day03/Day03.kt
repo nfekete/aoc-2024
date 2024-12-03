9bc53f45ebd0da96e3b0f066109acb8db12fc736
Nándor Előd Fekete <nandor.fekete@gmail.com>
2024-12-03 10:06:35 +0100

    Day 03 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2024/day03/Day03.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2024/day03/Day03.kt
@@ -2,9 +2,26 @@ package me.nfekete.adventofcode.y2024.day03
 
 import me.nfekete.adventofcode.y2024.common.classpathFile
 
-private val mulExpressionRegex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+private val mulExpressionRegex = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
+private val instructionRegex = Regex("""mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)""")
 
-private data class Mul(val a: Long, val b: Long) {
+sealed interface Instruction {
+    companion object {
+        fun parseAll(text: String) =
+            instructionRegex.findAll(text)
+                .map { it.groupValues }
+                .map { (instruction, a, b) ->
+                    when (instruction) {
+                        "do()" -> Do
+                        "don't()" -> Dont
+                        else -> Mul(a.toLong(), b.toLong())
+                    }
+                }
+                .toList()
+    }
+}
+
+private data class Mul(val a: Long, val b: Long) : Instruction {
     companion object {
         fun parseAll(text: String) =
             mulExpressionRegex.findAll(text)
@@ -14,11 +31,29 @@ private data class Mul(val a: Long, val b: Long) {
     }
 }
 
+private data object Do : Instruction
+private data object Dont : Instruction
+
+@JvmName("evaluateMul")
 private fun Collection<Mul>.evaluate() = sumOf { it.a * it.b }
+
+private data class EvalState(val evalEnabled: Boolean = true, val sum: Long = 0)
+
+private fun Collection<Instruction>.evaluate() =
+    fold(EvalState()) { state, instruction ->
+        with(state) {
+            when (instruction) {
+                is Mul -> copy(sum = sum + if (evalEnabled) instruction.a * instruction.b else 0)
+                is Do -> copy(evalEnabled = true)
+                is Dont -> copy(evalEnabled = false)
+            }
+        }
+    }.sum
 
 private fun main() {
     val input = classpathFile("day03/input.txt")
         .readText()
 
-    Mul.parseAll(input).also(::println).evaluate().also(::println)
+    Mul.parseAll(input).evaluate().also { println("Part1: $it") }
+    Instruction.parseAll(input).evaluate().also { println("Part2: $it") }
 }
