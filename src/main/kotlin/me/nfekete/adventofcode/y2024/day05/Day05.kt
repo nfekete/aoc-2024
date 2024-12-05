d025fff0a3f9f8b017121b6c9224b86072191e64
Nándor Előd Fekete <nandor.fekete@gmail.com>
2024-12-05 11:14:43 +0100

    Day 05 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2024/day05/Day05.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2024/day05/Day05.kt
@@ -3,18 +3,28 @@ package me.nfekete.adventofcode.y2024.day05
 import me.nfekete.adventofcode.y2024.common.chunkBy
 import me.nfekete.adventofcode.y2024.common.classpathFile
 
-private fun List<Pair<Int, Int>>.isSequenceOrderedCorrectly(pages: List<Int>) =
-    groupBy({ it.second }, { it.first }).mapValues { it.value.toSet() }
-        .isSequenceOrderedCorrectly(pages)
-
-private fun Map<Int, Set<Int>>.isSequenceOrderedCorrectly(pages: Collection<Int>): Boolean =
+private fun Map<Int, Set<Int>>.isSequenceOrderedCorrectly(pages: List<Int>): Boolean =
     if (pages.size <= 1)
         true
     else {
         val first = pages.first()
-        val rest = pages.drop(1).toSet()
+        val rest = pages.drop(1)
         val mustNotFollow = get(first) ?: emptySet()
         rest.none { it in mustNotFollow } && isSequenceOrderedCorrectly(rest)
+    }
+
+private fun Map<Int, Set<Int>>.fixSequence(pages: List<Int>): List<Int> =
+    if (pages.size <= 1)
+        pages
+    else {
+        val first = pages.first()
+        val rest = pages.drop(1)
+        val mustNotFollow = get(first) ?: emptySet()
+        val newFirst = rest.firstOrNull { it in mustNotFollow }
+        if (newFirst == null)
+            listOf(first) + fixSequence(rest)
+        else
+            fixSequence(listOf(newFirst) + first + (rest - newFirst))
     }
 
 private fun main() {
@@ -23,13 +33,20 @@ private fun main() {
         .chunkBy { it.isEmpty() }
         .toList()
         .let { (ruleStrings, pageSequenceStrings) ->
-            val rules =
-                ruleStrings.map { ruleString -> ruleString.split("|").map { it.toInt() }.let { (a, b) -> a to b } }
+            val rules = ruleStrings.map { ruleString ->
+                ruleString.split("|").map { it.toInt() }.let { (a, b) -> a to b }
+            }.groupBy({ it.second }, { it.first }).mapValues { it.value.toSet() }
             val pageSequences = pageSequenceStrings.map { pageSequence -> pageSequence.split(",").map { it.toInt() } }
 
             pageSequences
                 .filter { rules.isSequenceOrderedCorrectly(it) }
                 .sumOf { it[it.size / 2] }
                 .also { println("Part1: $it") }
+
+            pageSequences
+                .filter { !rules.isSequenceOrderedCorrectly(it) }
+                .map { rules.fixSequence(it) }
+                .sumOf { it[it.size / 2] }
+                .also { println("Part2: $it") }
         }
 }
