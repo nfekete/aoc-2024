3977d68187b64c08cb7a9fb83bb43b753f671e09
Nándor Előd Fekete <nandor.fekete@gmail.com>
2024-12-07 15:06:50 +0100

    Day 07 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2024/day07/Day07.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2024/day07/Day07.kt
@@ -19,6 +19,25 @@ private fun Pair<Long, List<Long>>.canBeSatisfied(): Boolean {
     return recurse(null, requirements)
 }
 
+private infix fun Long?.concat(other: Long) = ((this?.let { this.toString() } ?: "") + other.toString()).toLong()
+
+private fun Pair<Long, List<Long>>.canBeSatisfied2(): Boolean {
+    val result = first
+    val requirements = second
+
+    fun recurse(acc: Long?, numbers: List<Long>): Boolean {
+        return if (numbers.isEmpty())
+            acc == result
+        else
+            recurse((acc ?: 0) + numbers.first(), numbers.drop(1)) ||
+                    recurse((acc ?: 1) * numbers.first(), numbers.drop(1)) ||
+                    recurse(acc.concat(numbers.first()), numbers.drop(1))
+    }
+
+    return recurse(null, requirements)
+}
+
+
 private fun main() {
     val input = classpathFile("day07/input.txt")
         .readLines()
@@ -28,5 +47,6 @@ private fun main() {
                 .map2 { numberStrings -> numberStrings.split(' ').map { it.toLong() } }
         }
 
-    input.filter { it.canBeSatisfied() }.onEach { println(it) }.sumOf { it.first }.also { println("Part1: $it") }
+    input.filter { it.canBeSatisfied() }.sumOf { it.first }.also { println("Part1: $it") }
+    input.filter { it.canBeSatisfied2() }.sumOf { it.first }.also { println("Part2: $it") }
 }
