8877d1f3f32cf92906b23e1dd6f6d927845bb1c1
Nándor Előd Fekete <nandor.fekete@gmail.com>
2024-12-10 01:10:33 +0100

    Day 09 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2024/day09/Day09.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2024/day09/Day09.kt
@@ -2,24 +2,26 @@ package me.nfekete.adventofcode.y2024.day09
 
 import me.nfekete.adventofcode.y2024.common.classpathFile
 import java.util.*
+import kotlin.collections.ArrayDeque
 
-private data class FileBlock(val fileId: Int, val used: Int, val free: Int)
+private data class File(val fileId: Int, val used: Int, val free: Int)
+typealias Block = Int? // null means empty
 
 private fun String.parseDiskMap() = toList()
     .windowed(2, 2, true)
     .mapIndexed { index, chars ->
         if (chars.size == 1) {
-            FileBlock(index, chars[0].digitToInt(), 0)
+            File(index, chars[0].digitToInt(), 0)
         } else {
             val (usedS, freeS) = chars
-            FileBlock(index, usedS.digitToInt(), freeS.digitToInt())
+            File(index, usedS.digitToInt(), freeS.digitToInt())
         }
     }
 
-private fun List<FileBlock>.toBlocks() =
-    asSequence().flatMap { fileBlock -> List(fileBlock.used) { fileBlock.fileId } + List(fileBlock.free) { null } }
+private fun List<File>.toBlocks() =
+    flatMap { fileBlock -> List(fileBlock.used) { fileBlock.fileId } + List(fileBlock.free) { null } }
 
-private fun Sequence<Int?>.defragment(): Sequence<Int> = sequence {
+private fun List<Block>.defragment(): List<Int?> = sequence {
     val blocks = this@defragment.toCollection(LinkedList())
     while (blocks.isNotEmpty()) {
         val block = blocks.removeFirst()
@@ -33,13 +35,42 @@ private fun Sequence<Int?>.defragment(): Sequence<Int> = sequence {
             yield(last)
         }
     }
+}.toList()
+
+@JvmName("defragmentFiles")
+private fun List<File>.defragment(): List<Block> =
+    toBlocks().toMutableList().let { blocks ->
+        val queue = toCollection(ArrayDeque())
+        while (queue.isNotEmpty()) {
+            val file = queue.removeLast()
+            val newIndex = blocks.windowed(file.used, 1).indexOfFirst { window -> window.all { it == null } }
+            if (newIndex != -1) {
+                val oldIndex =
+                    blocks.windowed(file.used, 1).indexOfFirst { windows -> windows.all { it == file.fileId } }
+                if (newIndex < oldIndex) {
+                    (newIndex..<newIndex + file.used).forEach { blocks[it] = file.fileId }
+                    (oldIndex..<oldIndex + file.used).forEach { blocks[it] = null }
+                }
+            }
+        }
+        blocks
+    }
+
+
+private fun List<Int?>.checksum() = withIndex().fold(0L) { acc, (index, value) ->
+    if (value == null) acc else acc + index * value
 }
 
-private fun Sequence<Int>.checksum() = withIndex().fold(0L) { acc, (index, value) -> acc + index * value }
+private fun List<Block>.pretty() =
+    joinToString("") { block -> block?.toString() ?: "." }
 
 private fun main() {
+    "2333133121414131402".parseDiskMap().toBlocks().defragment().toList().checksum()
+        .also { println("Sample part 1: $it") }
+    "2333133121414131402".parseDiskMap().defragment()
+        .checksum().also { println("Sample part 2: $it") }
 
-//    "2333133121414131402".parseDiskMap().toBlocks().defragment().checksum().also { println("Sample: $it") }
     val input = classpathFile("day09/input.txt").readLine().parseDiskMap()
     input.toBlocks().defragment().checksum().also { println("Part 1: $it") }
+    input.defragment().checksum().also { println("Part 2: $it") }
 }
