c27a2e39340ea2769cd20f038caeffe975e37733
Nándor Előd Fekete <nandor.fekete@gmail.com>
2024-12-11 11:13:31 +0100

    Day 10 improvement: solve both parts in one step

--- src/main/kotlin/me/nfekete/adventofcode/y2024/day10/Day10.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2024/day10/Day10.kt
@@ -3,6 +3,7 @@ package me.nfekete.adventofcode.y2024.day10
 import me.nfekete.adventofcode.y2024.common.Grid2D
 import me.nfekete.adventofcode.y2024.common.Grid2D.Coord
 import me.nfekete.adventofcode.y2024.common.classpathFile
+import me.nfekete.adventofcode.y2024.common.mapBoth
 
 context(Grid2D<Char>)
 fun Coord.trails(visited: Set<Coord> = setOf()): Set<Collection<Coord>> {
@@ -14,21 +15,21 @@ fun Coord.trails(visited: Set<Coord> = setOf()): Set<Collection<Coord>> {
         .toSet()
 }
 
-private fun Grid2D<Char>.part1(): Int {
-    return map.entries.filter { (_, value) -> value == '0' }
-        .sumOf { (coord, _) -> coord.trails().map { it.last() }.distinct().size }
-}
-
-private fun Grid2D<Char>.part2(): Int {
-    return map.entries.filter { (_, value) -> value == '0' }
-        .sumOf { (coord, _) -> coord.trails().size }
-}
+private fun Grid2D<Char>.bothParts(): Pair<Int, Int> =
+    map.entries.filter { (_, value) -> value == '0' }
+        .map { (coord, _) ->
+            val trails = coord.trails()
+            trails.map { it.last() }.distinct().size to trails.size
+        }.unzip()
+        .mapBoth { it.sum() }
 
 private fun main() {
     val input = classpathFile("day10/input.txt")
         .readLines()
         .let { Grid2D.from(it) }
 
-    input.part1().also { println("Part1: $it") }
-    input.part2().also { println("Part2: $it") }
+    input.bothParts().also {
+        println("Part1: ${it.first}")
+        println("Part2: ${it.second}")
+    }
 }
