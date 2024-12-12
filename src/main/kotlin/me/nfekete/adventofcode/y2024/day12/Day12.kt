89210174eb0d72d9b6b8d81887624276dd00f034
Nándor Előd Fekete <nandor.fekete@gmail.com>
2024-12-12 16:06:59 +0100

    Day 12 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2024/day12/Day12.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2024/day12/Day12.kt
@@ -1,8 +1,16 @@
 package me.nfekete.adventofcode.y2024.day12
 
 import me.nfekete.adventofcode.y2024.common.Grid2D
+import me.nfekete.adventofcode.y2024.common.Grid2D.CardinalDirection.DOWN
+import me.nfekete.adventofcode.y2024.common.Grid2D.CardinalDirection.LEFT
+import me.nfekete.adventofcode.y2024.common.Grid2D.CardinalDirection.RIGHT
+import me.nfekete.adventofcode.y2024.common.Grid2D.CardinalDirection.UP
 import me.nfekete.adventofcode.y2024.common.Grid2D.Coord
+import me.nfekete.adventofcode.y2024.common.Grid2D.ObliqueDirection.DOWN_RIGHT
+import me.nfekete.adventofcode.y2024.common.Grid2D.ObliqueDirection.UP_RIGHT
 import me.nfekete.adventofcode.y2024.common.classpathFile
+import me.nfekete.adventofcode.y2024.day12.Axis.HORIZONTAL
+import me.nfekete.adventofcode.y2024.day12.Axis.VERTICAL
 
 context(Grid2D<Char>)
 private fun Set<Coord>.perimeter() =
@@ -21,15 +29,64 @@ private fun Coord.detectArea(visited: MutableSet<Coord> = mutableSetOf()): Set<C
         visited
     }
 
+private enum class Axis { HORIZONTAL, VERTICAL }
+private data class Wall(val coord: Coord, val axis: Axis)
+
+context(Grid2D<Char>)
+private fun Set<Coord>.detectSides() =
+    flatMap { coord ->
+        Grid2D.CardinalDirection.entries.mapNotNull { direction ->
+            val wall = when (direction) {
+                UP -> Wall(coord, HORIZONTAL)
+                DOWN -> Wall(coord + DOWN.delta, HORIZONTAL)
+                LEFT -> Wall(coord, VERTICAL)
+                RIGHT -> Wall(coord + RIGHT.delta, VERTICAL)
+            }
+            wall.takeIf { map[coord + direction.delta] != map[coord] }
+        }
+    }.let { walls ->
+        walls.map { wall ->
+            when (wall.axis) {
+                HORIZONTAL -> HORIZONTAL to wall.coord.y to wall.coord.x
+                VERTICAL -> VERTICAL to wall.coord.x to wall.coord.y
+            }
+        }.groupBy({ it.first }) { it.second }.let { map ->
+            val sides = map.mapValues { (key, positions) ->
+                val sorted = positions.sorted()
+                1 + sorted.windowed(2).count { (a, b) -> b - a != 1L }
+            }.values.sum()
+            val compensation = 2 * count { coord ->
+                (coord + DOWN.delta) !in this &&
+                        (coord + RIGHT.delta) !in this &&
+                        (coord + DOWN_RIGHT.delta) in this ||
+                        (coord + UP.delta) !in this &&
+                        (coord + RIGHT.delta) !in this &&
+                        (coord + UP_RIGHT.delta) in this
+            }
+            sides + compensation
+        }
+    }
+
 private data class Area(val plant: Char, val coords: Set<Coord>) {
     context(Grid2D<Char>)
-    val perimeter get() = coords.perimeter()
+    val perimeter
+        get() = coords.perimeter()
     val area get() = coords.size
     context(Grid2D<Char>)
-    val price get() = area * perimeter
+    val wallPrice
+        get() = area * perimeter
+    context(Grid2D<Char>)
+    val sides
+        get() = coords.detectSides()
+    context(Grid2D<Char>)
+    val sidePrice
+        get() = area * sides
 }
 
-private fun Grid2D<Char>.fencingPrice(): Int {
+private fun Grid2D<Char>.fencingPrice(priceExtractor: (Area) -> Int): Int =
+    areas().sumOf(priceExtractor)
+
+private fun Grid2D<Char>.areas(): MutableList<Area> {
     val remainingCoords = coords.toMutableSet()
     val areas = mutableListOf<Area>()
     while (remainingCoords.isNotEmpty()) {
@@ -38,7 +95,7 @@ private fun Grid2D<Char>.fencingPrice(): Int {
         remainingCoords -= area
         areas.add(Area(map.getValue(coord), area))
     }
-    return areas.sumOf { it.price }
+    return areas
 }
 
 private fun main() {
@@ -46,5 +103,8 @@ private fun main() {
         .readLines()
         .let { Grid2D.from(it) }
 
-    grid.fencingPrice().also { println("Part1: $it") }
+    with(grid) {
+        fencingPrice { it.wallPrice }.also { println("Part1: $it") }
+        fencingPrice { it.sidePrice }.also { println("Part2: $it") } // 886378 not correct
+    }
 }
